42227bd3d54c5d603ef965d95fb64ea738878c87
pekka <plaer-123@mail.ru>
2026-02-22 17:45:04 +0300

    fix history switching + image upload

--- mychat/server.js
+++ mychat/server.js
@@ -901,8 +901,7 @@ io.on('connection', (socket) => {
 
   socket.on('getPrivateHistory', async (data) => {
     var otherLogin = typeof data === 'string' ? data : data.login;
-    // Token is always required now - if missing (old call style), use 0 which won't match any valid token
-    var token = (typeof data === 'object' && data.token !== undefined) ? data.token : 0;
+    var token = (typeof data === 'object' && data.token !== undefined) ? data.token : -1; // -1 never matches client token
     if (!socket.userLogin) return;
     try {
       const res = await pool.query('SELECT * FROM private_messages WHERE (from_login=$1 AND to_login=$2) OR (from_login=$2 AND to_login=$1) ORDER BY timestamp ASC LIMIT 200', [socket.userLogin, otherLogin]);
